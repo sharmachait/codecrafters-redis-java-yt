dbe9e2a2cb739caa99232c7e42a1c7bf60bd3bba
sharmachait <chait8126@gmail.com>
2025-03-11 09:16:11 +0530

    DI

--- src/main/java/Components/TcpServer.java
+++ src/main/java/Components/TcpServer.java
@@ -1,16 +1,16 @@
-package Components;
-
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-@Component
-
-public class TcpServer {
-    @Autowired
-    private RespSerializer respSerializer;
-    public void startServer(){
-        respSerializer.printWorking();
-    }
-}
+//package Components;
+//
+//import lombok.NoArgsConstructor;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//
+//public class TcpServer {
+//    @Autowired
+//    private RespSerializer respSerializer;
+//    public void startServer(){
+//        respSerializer.printWorking();
+//    }
+//}
