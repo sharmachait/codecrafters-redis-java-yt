ec088b814b1cf0e31e761164bee5071376eaa267
sharmachait <chait8126@gmail.com>
2025-03-17 09:43:54 +0530

    resp deserializer

--- src/main/java/Components/RespSerializer.java
+++ src/main/java/Components/RespSerializer.java
@@ -2,12 +2,77 @@ package Components;
 
 import org.springframework.stereotype.Component;
 
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class RespSerializer {
-    public void printWokring(){
-        System.out.println("-------------------------------------------------------------------------------------------");
+    public int getParts(char []dataArr, int i, String[] subArray){
+        int j=0;
+        while(i< dataArr.length && j < subArray.length){
+            if(dataArr[i] == '$'){
+                //bulk String
+                //$<length>\r\n<data>\r\n
+                i++;
+                String partLength = "";
+                while(i < dataArr.length && Character.isDigit(dataArr[i])){
+                    partLength += dataArr[i];
+                    i++;
+                }
+                i+=2;
+                String part = "";
+                for(int k=0; k<Integer.parseInt(partLength);k++){
+                    part+=dataArr[i++];
+                }
+                i+=2;
+                subArray[j++]=part;
+            }
+        }
+        return i;
     }
-    public String returnsString(){
-        return "hi";
+
+    public List<String[]> deseralize(byte[] command){
+
+        String data = new String(command, StandardCharsets.UTF_8);
+        char[] dataArr = data.toCharArray();
+
+        List<String[]> res = new ArrayList<>();
+
+        int i=0;
+        while(i < dataArr.length){
+            char curr = dataArr[i];
+            if(curr == '*'){
+                //array
+                String arrLen = "";
+                i++;
+                while(i < dataArr.length && Character.isDigit(dataArr[i])){
+                    arrLen += dataArr[i++];
+                }
+                i+=2;
+                if(dataArr[i] == '*'){
+                    // *2
+                    // *3\r\n#3set\r\n#3key\r\n#5value
+                    // *3\r\n#3set\r\n#3key\r\n#5value
+                    for(int t=0;t<Integer.parseInt(arrLen);i++){
+                        String nestedLen = "";
+                        i++;
+                        while(i < dataArr.length && Character.isDigit(dataArr[i])){
+                            nestedLen += dataArr[i++];
+                        }
+                        i+=2;
+                        String[] subArray = new String[Integer.parseInt(nestedLen)];
+                        i = getParts(dataArr, i, subArray);
+                        res.add(subArray);
+                    }
+                }else{
+                    // *3\r\n#3set\r\n#3key\r\n#5value
+                    String[] subArray = new String[Integer.parseInt(arrLen)];
+                    i = getParts(dataArr, i, subArray);
+                    res.add(subArray);
+                }
+            }
+        }
+        return res;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,79 +1,14 @@
 import Components.TcpServer;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.Scanner;
-import java.util.concurrent.CompletableFuture;
 
 public class Main {
 
-  public static void main(String[] args){
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
+    public static void main(String[] args){
       AnnotationConfigApplicationContext context =
               new AnnotationConfigApplicationContext(AppConfig.class);
       TcpServer app = context.getBean(TcpServer.class);
-
       app.startServer();
-    //  Uncomment this block to pass the first stage
-        ServerSocket serverSocket = null;
-        Socket clientSocket = null;
-        int port = 6379;
-        try {
-          serverSocket = new ServerSocket(port);
-          serverSocket.setReuseAddress(true);
-
-            while (true) {
-                clientSocket = serverSocket.accept();
-                Socket finalClientSocket = clientSocket;
-                CompletableFuture.runAsync(() -> {
-                    try {
-                        handleClient(finalClientSocket);
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                });
-            }
-
-        } catch (IOException e) {
-          System.out.println("IOException: " + e.getMessage());
-        } finally {
-          try {
-            if (clientSocket != null) {
-                clientSocket.close();
-            }
-          } catch (IOException e) {
-            System.out.println("IOException: " + e.getMessage());
-          }
-        }
-  }
-
-    private static void handleClient(Socket clientSocket) throws IOException {
-        InputStream inputStream = clientSocket.getInputStream();
-        OutputStream outputStream = clientSocket.getOutputStream();
-        Scanner sc = new Scanner(inputStream);
-//        sc.useDelimiter("\r\n\r\n\r\n\r\n");
-
-        while(sc.hasNextLine()){
-            String nextLine = sc.next();
-            System.out.println("=====================================================================================================");
-            System.out.println(nextLine);
-            System.out.println("=====================================================================================================");
-//            if(nextLine.contains("PING")){
-//                outputStream.write("+PONG\r\n".getBytes());
-//            }
-//            if(nextLine.contains("ECHO")){
-//                String respHeader = sc.nextLine();
-//                String respBody = sc.nextLine();
-//                String response = respHeader + "\r\n" + respBody+"\r\n";
-//                outputStream.write(response.getBytes());
-//            }
-        }
-
     }
 
     public static String encodingRespString(String s){
