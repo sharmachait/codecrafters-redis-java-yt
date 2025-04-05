650d2f7a0828be3de0b71bb8d3ff83c0da9f6593
sharmachait <chait8126@gmail.com>
2025-04-05 18:59:39 +0530

    ye5

--- src/main/java/Components/Server/RedisConfig.java
+++ src/main/java/Components/Server/RedisConfig.java
@@ -4,10 +4,27 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.stereotype.Component;
 
-@Getter
-@Setter
+//@Getter
+//@Setter
 @Component
 public class RedisConfig {
+    public String getRole() {
+        return role;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
     public String role;
     public int port;
+
 }
