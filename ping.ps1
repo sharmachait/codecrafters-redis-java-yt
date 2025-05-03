2d9d7ea11244e3fc58d34d9d32bd19b29e31e2f2
sharmachait <chait8126@gmail.com>
2025-05-04 02:29:38 +0530

    docker file and ping test script

---
+++ ping.ps1
@@ -0,0 +1,13 @@
+﻿$client = New-Object System.Net.Sockets.TcpClient('localhost', 6379)
+$stream = $client.GetStream()
+$writer = New-Object System.IO.StreamWriter($stream)
+$writer.AutoFlush = $true
+$reader = New-Object System.IO.StreamReader($stream)
+$pingcommand = "*1`r`n`$4`r`nPING`r`n"
+$writer.Write($pingcommand)
+$response = $reader.ReadLine()
+Write-Host "Server Response: $response"
+
+$reader.Close()
+$writer.Close()
+$client.Close()
